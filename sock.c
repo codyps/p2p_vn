d4edd90f046364127b711d4a152fd70000ade75d
Cody Schafer <cpschafer@gmail.com>
2010-09-25 22:36:24 -0400

    support the require cmd line interface.

--- sock.c
+++ sock.c
@@ -17,7 +17,115 @@ struct peer_data {
 	struct addrinfo *res;
 };
 
+struct peer_array {
+	size_t pct;
+	struct peer_data *pd;
+};
+
+#define DIE(...) do {                                   \
+	fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+	fprintf(stderr, __VA_ARGS__);                   \
+	fputc('\n', stderr);                            \
+	exit(EXIT_FAILURE);                             \
+} while (0)
+
+static struct peer_array *peer_array_mk(void)
+{
+	struct peer_array *p = malloc(sizeof(*p));
+	if (!p) {
+		DIE("no mem for peer_array_mk");
+	}
+
+	memset(p, 0, sizeof(p));
+	return p;
+}
+
+static void peer_add(struct peer_array *pa, char *name, char *port)
+{
+	pa->pct ++;
+	pa->pd = realloc(pa->pd, sizeof(*pa->pd) * pa->pct);
+	memset(pa->pd + pa->pct - 1, 0, sizeof(*pa->pd));
+	pa->pd[pa->pct - 1].name = name;
+	pa->pd[pa->pct - 1].port = port;
+}
+
+static void usage(const char *name)
+{
+	fprintf(stderr,
+		"usage: %s <port> <local interface>\n"
+		"       %s <remote host> <remote port> <local interface>\n"
+		, name, name);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
+{
+	char *listen_port;
+	char *l_if;
+	struct peer_array *peers = peer_array_mk();
+
+	if (argc == 3) {
+		/* listener */
+
+		listen_port = argv[1];
+		l_if = argv[2];
+	} else if (argc == 4) {
+		/* connector */
+
+		listen_port = DEFAULT_PORT_STR;
+		l_if = argv[3];
+
+		char *rname = argv[1];
+		char *rport = argv[2];
+		peer_add(peers,rname,rport);
+	} else {
+		usage((argc>0)?argv[0]:"L203");
+	}
+
+	fprintf(stderr, "we have %zu peers:\n", peers->pct);
+	size_t i;
+	for (i = 0; i < peers->pct; i++) {
+		fprintf(stderr, " name: %s:%s\n", peers->pd[i].name,
+				peers->pd[i].port);
+	}
+
+	/* TODO: bind */
+
+	/* seed-peer data population */
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_NUMERICSERV;
+
+	for (i = 0; i < peers->pct; i++) {
+		int r = getaddrinfo(peers->pd[i].name,
+				peers->pd[i].port, &hints,
+				&peers->pd[i].res);
+		if (r) {
+			fprintf(stderr, "whoops: %s: %d %s\n",
+					peers->pd[i].name,
+					r, gai_strerror(r));
+		}
+	}
+
+	/* TODO: connect to peers */
+
+	/* TODO: main loop { */
+
+	/* TODO: deal with new incomming conections */
+	/* TODO: form outgoing connections */
+	/* TODO: deal with data from connected peers */
+	/* TODO: request data from connected peers */
+	/* TODO: send data to peers */
+
+	/* } */
+
+	return 0;
+}
+
+#if 0
+int complex_parse(int argc, char **argv)
 {
 	char *listen_port;
 	struct peer_data *peers = 0;
@@ -51,44 +159,5 @@ int main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 	}
-
-	fprintf(stderr, "we have %zu peers:\n", peer_ct);
-	size_t i;
-	for (i = 0; i < peer_ct; i++) {
-		fprintf(stderr, " name: %s:%s\n", peers[i].name, peers[i].port);
-	}
-
-	/* TODO: bind */
-
-	/* seed-peer data population */
-	struct addrinfo hints;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_NUMERICSERV;
-
-	for (i = 0; i < peer_ct; i++) {
-		int r = getaddrinfo(peers[i].name, peers[i].port, &hints,
-				&peers[i].res);
-		if (r) {
-			fprintf(stderr, "whoops: %s: %d %s\n",
-					peers[i].name,
-					r, gai_strerror(r));
-		}
-	}
-
-	/* TODO: connect to peers */
-
-	/* TODO: main loop { */
-
-	/* TODO: deal with new incomming conections */
-	/* TODO: form outgoing connections */
-	/* TODO: deal with data from connected peers */
-	/* TODO: request data from connected peers */
-	/* TODO: send data to peers */
-
-	/* } */
-
-	return 0;
 }
-
+#endif
