95af9b0e6241df74d1b5f71a80644c0b6fca4c5b
Cody Schafer <cpschafer@gmail.com>
2010-10-16 03:31:32 -0400

    alias_ip: demonstrate getting netmask, update filter.

--- alias_ip.c
+++ alias_ip.c
@@ -23,19 +23,28 @@
  (009) ret      #65535
  */
 
-/* presently this does "\(not ip \) or \( ip src 192.168.0.1 \)" */
+
+#if 0
+/* __SUBNET_MASK__ and __SUBNET_VAL__ need to be replaced with actual values
+ * (at runtime)
+ *
+ * Note: BPF_LD converts things to host byte order.
+ */
 struct sock_filter sf[] {
 	/* Load h_proto in ethernet header */
 	BPF_STMT(BPF_LD | BPF_H | BPF_ABS, offsetof(struct ethhdr, h_proto)),
 
 	/* Check for IP packets. T = next, F = done succ. */
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, ETH_P_IP, 1, 4),
+	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, ETH_P_IP, 1, 5),
 
 	/* load ip src */
 	BPF_STMT(BPF_LD | BPF_W | BPF_ABS, ETH_HLEN + offsetof(struct iphdr, saddr)),
 
+	/* And it to compare for subnet */
+	BPF_STMT(BPF_ALU | BPF_AND, __SUBNET_MASK__),
+
 	/* is ipsrc == the filtered address? */
-	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, 0, 1, 2),
+	BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __SUBNET_VAL__, 1, 2),
 
 	/* accept entire packet */
 	BPF_STMT(BPF_RET | BPF_K, -1),
@@ -43,6 +52,7 @@ struct sock_filter sf[] {
 	/* reject packet */
 	BPF_STMT(BPF_RET | BPF_K, 0)
 };
+#endif
 
 int main(int argc, char **argv)
 {
@@ -59,7 +69,6 @@ int main(int argc, char **argv)
 
 	struct sockaddr_in *addr = (struct sockaddr_in *)&(req.ifr_addr);
 
-
 	int ret = ioctl(sock, SIOCGIFADDR, &req);
 
 	if (ret < 0) {
@@ -68,6 +77,16 @@ int main(int argc, char **argv)
 	}
 
 	char ipbuf[256];
+	if (!inet_ntop(AF_INET, &addr->sin_addr, ipbuf, sizeof(ipbuf))) {
+		fprintf(stderr, "inet_ntop fail: %s\n", strerror(errno));
+		return -1;
+	}
+
+	printf("%s = %s = 0x%x\n", argv[1], ipbuf, ntohl(addr->sin_addr.s_addr));
+
+
+	ret = ioctl(sock, SIOCGIFNETMASK, &req);
+
 	if (!inet_ntop(AF_INET, &addr->sin_addr, ipbuf, sizeof(ipbuf))) {
 		fprintf(stderr, "inet_ntop fail: %s\n", strerror(errno));
 		return -1;
