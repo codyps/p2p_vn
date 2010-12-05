c01bef62711a486c68c224cc98fb4ded1de901a8
Steven_Liberto <sliberto@cs352-34.rutgers.edu>
2010-12-05 17:39:17 -0500

    fix

--- dpg.c
+++ dpg.c
@@ -18,8 +18,18 @@ int dpg_init(dpg_t *g) {
 /*0 succes, < 0 fail, 1 on duplicate */
 int dpg_insert(dpg_t *g, direct_peer_t *dp) {
 	direct_peer_t dup;
-	dup = bsearch( , g->grp, g->num_peer, sizeof(struct direct_peer),
+	/*re sort after search */
+	if(g->num_peer) > 0) {
+		dup = bsearch( dp, g->grp, g->num_peer, 
+			sizeof(struct direct_peer), dp_cmp);
+		qsort(g->grp, g->num_peer, sizeof(struct direct_peer), dp_cmp);
+				
+		if(dup) {
+			return 1;
+		}
 		
+	}
+			
 	
 	
 	if(g->num_peer < g->size - 1) {
@@ -42,6 +52,8 @@ int dpg_remove(dpg_t *g, direct_peer_t *dp) {
 	int x;
 	int in;
 	direct_peer_t temp;
+	
+	
 	for(x = 0; x < g->count; x++) {
 		if(cmp_mac(DPEER_MAC(g->grp[x]), DPEER_MAC(dp)) == 2) {
 			g->grp[x] = g->grp[count -1];
@@ -51,4 +63,20 @@ int dpg_remove(dpg_t *g, direct_peer_t *dp) {
 		}
 	}
 	return -1;
+}
+
+/*return 0 if equal*/
+int dp_cmp(direct_peer_t p1, direct_peer_t p2) {
+	int x;
+	ether_addr_t a1 = DPEER_MAC(p1);
+	ether_addr_t a2 = DPEER_MAC(p2);	
+	for(x = 0; x < ETH_ALEN; x++) {
+		if(a1[x] > a2[x]) {
+			return 1;
+		}
+		if( a1[x] < a2[x]) {
+			return -1;
+		}
+	}
+	return 0;
 }
