57a4066a0898510b5bac139113b7c4cf3b0cfe9c
Cody Schafer <cschafer@gmail.com>
2010-12-05 18:14:32 -0500

    dpg & dp compile error squashing

--- dpeer.h
+++ dpeer.h
@@ -17,28 +17,24 @@ typedef struct direct_peer {
 
 	pthread_t dp_th;
 
-	ether_addr_t remote_mac;
-	struct sockaddr_in addr;
-	uint32_t rtt;
+	//ether_addr_t remote_mac;
+	//struct sockaddr_in addr;
+	//uint32_t rtt;
 
-	dpg_t *dpg;
+	//dpg_t *dpg;
 	routing_t *rd;
 	vnet_t *vnet;
-} direct_peer_t;
-
-typedef direct_peer_t dp_t;
-
+} dp_t;
 
 typedef uint32_t __be32;
 typedef uint16_t __be16;
-
 
 #define dp_from_eth(eth) container_of(eth, struct direct_peer, remote_mac);
 
 /* sends a data packet.
  * for use by the vnet thread
  */
-int dp_send_data(direct_peer_t *dp, void *data, size_t len);
+int dp_send_data(dp_t *dp, void *data, size_t len);
 
 /* each dp_init thread initializes the dp data structure to a degree,
  * and spawns a thread to complete initialization.
@@ -46,17 +42,17 @@ int dp_send_data(direct_peer_t *dp, void *data, size_t len);
  */
 
 /* for the command line specified peer */
-int dp_init_initial(direct_peer_t *dp,
+int dp_init_initial(dp_t *dp,
 		dpg_t *dpg, routing_t *rd, vnet_t *vnet,
 		char *host, char *port);
 
 /* peers recieved via link state packets. */
-int dp_init_linkstate(direct_peer_t *dp,
+int dp_init_linkstate(dp_t *dp,
 		dpg_t *dpg, routing_t *rd, vnet_t *vnet,
 		ether_addr_t mac, __be32 inet_addr, __be16 inet_port);
 
 /* incomming peer connections to the peer_listener */
-int dp_init_incoming(direct_peer_t *dp,
+int dp_init_incoming(dp_t *dp,
 		dpg_t *dpg, routing_t *rd, vnet_t *vnet,
 		int fd, struct sockaddr *addr);
 
