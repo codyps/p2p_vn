065c77aeb4b07c3eb0e1f626cc4d2900646a9b69
Cody Schafer <cschafer@gmail.com>
2010-12-04 19:17:20 -0500

    routing api: update to reflect difference between dhosts & ihosts

--- routing.h
+++ routing.h
@@ -48,15 +48,21 @@ int rt_init(routing_t *rd);
  * thread safe: no */
 void rt_destroy(routing_t *rd);
 
-/* adds a host with no links. not the best choice. */
-int rt_add_host(routing_t *rd, ether_addr_t mac);
+/* adds a host with no links.
+ * Intended for use in adding the 'root' direct peer (us) */
+int rt_dhost_add(routing_t *rd, ether_addr_t mac);
 
-/* add a link. Will create hosts if they do not exsist.
+/* add a link to a direct peer. Intended for use when a new connection is
+ * established.
+ *
+ * Will create dst_node if it does not exsist.
  * if link exsists, rtt is updated */
-int rt_add_link(routing_t *rd, ether_addr_t src_mac,
+int rt_dhost_add_link(routing_t *rd, ether_addr_t src_mac,
 		ether_addr_t dst_mac, uint64_t rtt);
 
-/* sets the links for a given node.
+/* sets the links for a given node. Routing copies specified data,
+ * it may be freed following this call's completion.
+ *
  * if link exsists, rtt is updated*/
 int rt_set_link(routing_t *rd, ether_addr_t src_mac,
 		ether_addr_t **dst_macs, uint64_t *rtts, size_t len);
@@ -64,8 +70,13 @@ int rt_set_link(routing_t *rd, ether_addr_t src_mac,
 /* also purges all links to/from this node */
 int rt_remove_host(routing_t *rd, ether_addr_t mac);
 
+/* *res is set to a list of rt_hosts. */
+int rt_dhost_get(routing_t *rd, struct rt_hosts **res);
+
 /* this allows us to have the packet be sent to multiple places,
  * allowing multicast to function properly.
+ * 
+ * Only returns dhosts.
  * *res is set to a list of rt_hosts. */
 int rt_hosts_to_host(routing_t *rd,
 		ether_addr_t src_mac, ether_addr_t dst_mac,
