058c58e7e1bbece12ff922de7486590efadd499b
Cody Schafer <cpschafer@gmail.com>
2010-12-13 19:57:48 -0500

    proto: document new link state packet.

--- peer_proto.h
+++ peer_proto.h
@@ -15,10 +15,14 @@ enum pkt_type {
 	PT_JOIN = 0xab01,
 	PT_PART = 0xab01,
 
-	/* it is unclear what differentiates this from part */
+	/* spec indicates that upon reciept, proxy should die */
 	PT_QUIT = 0xab03,
+
 	/* link state */
-	PT_LINK = 0xabaa,
+	PT_LINK = 0xabaa, /* the old format, flooded */
+
+	/* the new format, only sent to direct peers */
+	PT_LINK_GRAPH = 0xabac,
 
 	/* probes */
 	PT_PROBE_REQ = 0xab34,
@@ -38,6 +42,9 @@ enum pkt_type {
 	PT_DATA_ENC = 0xabc2,
 	PT_LINK_ENC = 0xabab,
 
+	/* rtt will respond to saturated links by decreasing, meaning we'll
+	 * begin to schedule packets over other paths, making explicit
+	 * bandwidth measurment moot */
 	PT_BWPROBE_REQ = 0xab35,
 	PT_BWPROBE_RESP = 0xab36
 } __packed __aligned;
@@ -54,6 +61,9 @@ enum pkt_len {
 
 	PL_LINK_STATIC = 2 + _PL_HOST + 1,
 	PL_NEIGHBOR = _PL_HOST + 4 + 8,
+
+	PL_LINK_GRAPH_STATIC = 2 + 2 * _PL_HOST,
+	PL_EDGE = _PL_HOST * 2 + 4 + 8
 } __packed __aligned;
 
 struct pkt_header {
@@ -91,14 +101,11 @@ struct pkt_quit {
 
 struct _pkt_neighbor {
 	struct _pkt_ipv4_host host;
-	uint32_t rtt_us; /* rtt in us */
-
-	/* XXX: ignore this. trusting other system's
-	 * clocks will not end well */
-	/* timestamp in ms */
+	uint32_t rtt_us;
 	uint64_t ts_ms;
 } __packed;
 
+/* link packet "old" */
 struct pkt_link {
 	uint16_t neighbor_ct;
 	struct _pkt_ipv4_host vec_src_host;
@@ -107,6 +114,22 @@ struct pkt_link {
 	uint8_t ttl;
 
 	struct _pkt_neighbor neighbors[];
+} __packed __aligned;
+
+struct _pkt_edge {
+	struct _pkt_ipv4_host src;
+	struct _pkt_ipv4_host dst;
+	uint32_t rtt_us;
+	uint64_t ts_ms;
+} __packed;
+
+/* link packet "new" */
+struct pkt_link_graph {
+	/* ignore, calculate via packet len */
+	uint16_t edge_ct;
+	struct _pkt_ipv4_host vec_src_host;
+
+	struct _pkt_edge edges[];
 } __packed __aligned;
 
 struct pkt_probe_req {
