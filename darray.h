1bc2b8114cab958bc7c2ddf307e5401df43d7f84
Cody Schafer <cpschafer@gmail.com>
2010-12-31 15:08:54 -0500

    darray: start work on parameterized function generation."

--- darray.h
+++ darray.h
@@ -3,6 +3,67 @@
 
 #include <stdbool.h>
 
+#define DA_DEF_TYPE(type)    \
+	struct da_##type {   \
+		size_t ct;   \
+		size_t mem;  \
+		type *items; \
+	}
+
+#define DA_INIT_MEM 8
+
+#define DA_DEF_FUNC(name, type, cmp, fattr) \
+	fattr DA_DEF_INIT(type)             \
+	fattr DA_DEF_REMOVE(type, cmp)      \
+	fattr DA_DEF_INSERT(type, cmp)      \
+	fattr DA_DEF_SEARCH(type, cmp)      \
+
+#define da_t(type) struct da_##type
+
+/* the parameter `name` needs to embody both the cmp and type. */
+#define da_remove(name) da_remove_##name
+#define da_init(name) da_init_##name
+#define da_search(name) da_search_##name
+#define da_insert(name) da_insert_##name
+
+#define DA_DEF_INIT(name, type)                                    \
+	int da_init(name)(da_t(type) *da) {                        \
+		da->ct = 0;                                        \
+		da->mem = 8;                                       \
+		da->items = malloc(sizeof(*da->items) * da->mem);  \
+		if (!da->items)                                    \
+			return -1;                                 \
+		return 0;                                          \
+	}
+
+#define DEF_BSEARCH(name, type, cmp)				\
+	type *bsearch_##name(type const *key, type const *base,	\
+			size_t nmemb) {				\
+		size_t l, u, idx;				\
+		type const *p;					\
+		int comparison;					\
+								\
+		l = 0;						\
+		u = nmemb;					\
+		while (l < u) {					\
+			idx = (l + u) / 2;			\
+			p = base + idx;				\
+			comparison = cmp(key, p);		\
+			if (comparison < 0)			\
+				u = idx;			\
+			else if (comparison > 0)		\
+				l = idx + 1;			\
+			else					\
+				return (type *)p;		\
+		}						\
+		return NULL;					\
+	}
+
+#define DA_DEF_REMOVE(name, type, cmp)                                \
+	int da_remove(name)(type *base, type *key) {                  \
+		type *found = bsearch(key, base, sizeof(*base), cmp); \
+	}\
+
 #define DA_CHECK_AND_REALLOC(mem_base, mem_sz, new_elem_ct)       \
 	({ bool fail;                                             \
 	if ((new_elem_ct) > (mem_sz)) {                           \
